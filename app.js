ff4a01b2dfa88b6b4e5a721281ba035fba41c4da
Felix Buenen <felixbuenen@hotmail.com>
2019-03-27 20:14:56 +0100

    start with session

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-const express = require('express')
+const express = require("express");
 const db = require("./app/database.js");
-const app = express()
-const port = 8050
+const app = express();
+const port = 8050;
+const expressSession = require("express-session");
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// register session middleware
+app.use(expressSession({ secret: "some-safe-secret" }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post("/login", (req, res) => {
+  res.send("<p>Logged in</p>");
+  res.end();
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/html/index.html");
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/html/debugLogin.html");
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- app/database.js
+++ app/database.js
@@ -4,117 +4,149 @@ let sqlite = require("sqlite3").verbose();
 let dbFile = __dirname + "/database.db";
 let fileExists = fs.existsSync(dbFile);
 
-if(!fileExists) {
-    fs.openSync(dbFile, "w");
+if (!fileExists) {
+  fs.openSync(dbFile, "w");
 }
 
 let db = new sqlite.Database(dbFile);
 
 db.serialize(function() {
-    if(!fileExists) {
-        setupDB();
-    }
+  if (!fileExists) {
+    setupDB();
+  }
 
-    // example query
-    db.each("SELECT name FROM Publishers, Books WHERE Publishers.rowid = Books.publisherID AND Books.title = 'Harry Potter and the Prisoner'", (err, row) => {
-        console.log("The publisher of Harry Potter and the Prisoner is: " + row.name);
-    })
-})
+  // example query
+  db.each(
+    "SELECT name FROM Publishers, Books WHERE Publishers.rowid = Books.publisherID AND Books.title = 'Harry Potter and the Prisoner'",
+    (err, row) => {
+      console.log(
+        "The publisher of Harry Potter and the Prisoner is: " + row.name
+      );
+    }
+  );
+});
 
 db.close();
 
 function setupDB() {
-    initBookTable();
-    initUserTable();
-    initPurchasesTable();
-    initRatings();
-    initReviews();
-    initAuthors();
-    initPublishers();
+  initBookTable();
+  initUserTable();
+  initPurchasesTable();
+  initRatings();
+  initReviews();
+  initAuthors();
+  initPublishers();
 }
 
 function initBookTable() {
-    db.run("CREATE TABLE Books (title TEXT, authorID INT, publisherID INT, genre TEXT, image TEXT, price REAL, nrRatings INT, rating FLOAT)");
+  db.run(
+    "CREATE TABLE Books (title TEXT, authorID INT, publisherID INT, genre TEXT, image TEXT, price REAL, nrRatings INT, rating FLOAT)"
+  );
 
-    let stmt = db.prepare("INSERT INTO Books VALUES (?,?,?,?,?,?,?,?)");
+  let stmt = db.prepare("INSERT INTO Books VALUES (?,?,?,?,?,?,?,?)");
 
-    // example data
-    stmt.run("Harry Potter and the Prisoner", 1, 1, "Fantasy", "./images/hp.png", 14.99, 50, 4.3);
-    stmt.run("Doom's Day", 37, 1, "War", "./images/dd.png", 11.99, 24, 3.6);
+  // example data
+  stmt.run(
+    "Harry Potter and the Prisoner",
+    1,
+    1,
+    "Fantasy",
+    "./images/hp.png",
+    14.99,
+    50,
+    4.3
+  );
+  stmt.run("Doom's Day", 37, 1, "War", "./images/dd.png", 11.99, 24, 3.6);
 
-    stmt.finalize();
+  stmt.finalize();
 }
 
 function initUserTable() {
-    db.run("CREATE TABLE Users (firstName TEXT, lastName TEXT, email TEXT, password TEXT)");
+  db.run(
+    "CREATE TABLE Users (firstName TEXT, lastName TEXT, email TEXT, password TEXT)"
+  );
 
-    let stmt = db.prepare("INSERT INTO Users VALUES (?,?,?,?)");
+  let stmt = db.prepare("INSERT INTO Users VALUES (?,?,?,?)");
 
-    // example data
-    stmt.run("Felix", "Buenen", "felixbuenen@hotmail.com", "encryptedpassword");
-    stmt.run("Desiree", "Van Den Braak", "desireevdbraak@gmail.com", "1234_whoo");
+  // example data
+  stmt.run("Felix", "Buenen", "felixbuenen@hotmail.com", "encryptedpassword");
+  stmt.run("Desiree", "Van Den Braak", "desireevdbraak@gmail.com", "1234_whoo");
 
-    stmt.finalize();
+  stmt.finalize();
 }
 
 function initPurchasesTable() {
-    db.run("CREATE TABLE Purchases (bookID INT, userID INT, date DATETIME)");
+  db.run("CREATE TABLE Purchases (bookID INT, userID INT, date DATETIME)");
 
-    let stmt = db.prepare("INSERT INTO Purchases VALUES (?,?,?)");
+  let stmt = db.prepare("INSERT INTO Purchases VALUES (?,?,?)");
 
-    // example data
-    stmt.run(1, 0, "2014-04-26 16:21:53");
-    stmt.run(0, 1, "2016-12-21 09:36:01");
+  // example data
+  stmt.run(1, 0, "2014-04-26 16:21:53");
+  stmt.run(0, 1, "2016-12-21 09:36:01");
 
-    stmt.finalize();
+  stmt.finalize();
 }
 
 function initRatings() {
-    db.run("CREATE TABLE Ratings (userID INT, bookID INT, rating TINYINT)");
+  db.run("CREATE TABLE Ratings (userID INT, bookID INT, rating TINYINT)");
 
-    let stmt = db.prepare("INSERT INTO Ratings VALUES (?,?,?)");
+  let stmt = db.prepare("INSERT INTO Ratings VALUES (?,?,?)");
 
-    // example data
-    stmt.run(0, 0, 5);
-    stmt.run(0, 1, 4);
-    stmt.run(1, 0, 5);
-    stmt.run(1, 1, 2);
+  // example data
+  stmt.run(0, 0, 5);
+  stmt.run(0, 1, 4);
+  stmt.run(1, 0, 5);
+  stmt.run(1, 1, 2);
 
-    stmt.finalize();    
+  stmt.finalize();
 }
 
 function initReviews() {
-    db.run("CREATE TABLE Reviews (userID INT, bookID INT, content TEXT, date DATETIME, ananymous BOOLEAN)");
+  db.run(
+    "CREATE TABLE Reviews (userID INT, bookID INT, content TEXT, date DATETIME, ananymous BOOLEAN)"
+  );
 
-    let stmt = db.prepare("INSERT INTO Reviews VALUES (?,?,?,?,?)");
+  let stmt = db.prepare("INSERT INTO Reviews VALUES (?,?,?,?,?)");
 
-    // example data
-    stmt.run(0, 0, "This is a review book was good groetjes.", "2019-06-17 16:21:23", 0);
-    stmt.run(1, 1, "Another review didn't like the book so I want to be ananymous", "2011-08-42 12:26:21", 1);
+  // example data
+  stmt.run(
+    0,
+    0,
+    "This is a review book was good groetjes.",
+    "2019-06-17 16:21:23",
+    0
+  );
+  stmt.run(
+    1,
+    1,
+    "Another review didn't like the book so I want to be ananymous",
+    "2011-08-42 12:26:21",
+    1
+  );
 
-    stmt.finalize();    
+  stmt.finalize();
 }
 
 function initAuthors() {
-    db.run("CREATE TABLE Authors (firstName TEXT, lastName TEXT, image TEXT)");
+  db.run("CREATE TABLE Authors (firstName TEXT, lastName TEXT, image TEXT)");
 
-    let stmt = db.prepare("INSERT INTO Authors VALUES (?,?,?)");
+  let stmt = db.prepare("INSERT INTO Authors VALUES (?,?,?)");
 
-    // example data
-    stmt.run("J.", "K. Rowling", "./images/jk-rowling.png");
-    stmt.run("Frank", "Herbert", "./images/herbert.png");
+  // example data
+  stmt.run("J.", "K. Rowling", "./images/jk-rowling.png");
+  stmt.run("Frank", "Herbert", "./images/herbert.png");
 
-    stmt.finalize();    
+  stmt.finalize();
 }
 
 function initPublishers() {
-    db.run("CREATE TABLE Publishers (name TEXT, country TEXT, city TEXT)");
+  db.run("CREATE TABLE Publishers (name TEXT, country TEXT, city TEXT)");
 
-    let stmt = db.prepare("INSERT INTO Publishers VALUES (?,?,?)");
+  let stmt = db.prepare("INSERT INTO Publishers VALUES (?,?,?)");
 
-    // example data
-    stmt.run("Bloomsbury", "England", "London");
-    stmt.run("Books LTD", "The Netherlands", "Utrecht");
+  // example data
+  stmt.run("Bloomsbury", "England", "London");
+  stmt.run("Books LTD", "The Netherlands", "Utrecht");
 
-    stmt.finalize();    
+  stmt.finalize();
 }
