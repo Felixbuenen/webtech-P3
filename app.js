ab2657f186edda3147f2aeec30b41d869539e38a
Felix Buenen <felixbuenen@hotmail.com>
2019-04-01 20:51:03 +0200

    morgan logger

--- app.js
+++ app.js
@@ -5,21 +5,42 @@ const port = 8050;
 const expressSession = require("express-session");
 const bodyParser = require("body-parser");
 const { storeUser, User } = require("./app/scripts/database-store");
+const path = require("path");
+const fs = require("fs"); // file system
+const morgan = require("morgan"); // Morgan logging
 
 global.sess; // session variable
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// Morgan logger
+var logToFileStream = fs.createWriteStream(
+  path.join(__dirname, "./access.log"),
+  { flags: "w" }
+);
+app.use(
+  morgan("common", {
+    stream: {
+      write: function(str) {
+        logToFileStream.write(str + "\r\n");
+      }
+    }
+  })
+);
+
+// register body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // register session middleware
-app.use(expressSession({ 
-  secret: "some-safe-secret",
-  resave: true,
-  saveUninitialized: true,
-  cookie: {
-    httpOnly: true,
-    maxAge: 5 * 24 * 60 * 60 * 1000 // 5 days
-  }
- }));
+app.use(
+  expressSession({
+    secret: "some-safe-secret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      maxAge: 5 * 24 * 60 * 60 * 1000 // 5 days
+    }
+  })
+);
 
 // register static file serving
 app.use(express.static(__dirname + "/public/"));
@@ -34,39 +55,35 @@ app.post("/register", (req, res) => {
   const email = req.body.email;
   const pass = req.body.password;
 
-  const db = require('./app/scripts/database-init');
-  db.get(
-    "SELECT * FROM Users WHERE email='" + email + "'",
-    (err, row) => {
-      if(err != undefined) {
-        console.log(err);
-        res.send("an error occured");
-        return;
-      }
-
-      // email exists --> redirect user to register page
-      if(row) {
-        res.redirect("register.html");
-        return;
-      }
-    
-      global.sess = req.session;
-    
-      global.sess.fname = fname;
-      global.sess.lname = lname;
-    
-      storeUser(new User(fname, lname, email, pass));
-    
-      res.redirect("/");
-      res.end();
+  const db = require("./app/scripts/database-init");
+  db.get("SELECT * FROM Users WHERE email='" + email + "'", (err, row) => {
+    if (err != undefined) {
+      console.log(err);
+      res.send("an error occured");
+      return;
     }
-  );
+
+    // email exists --> redirect user to register page
+    if (row) {
+      res.redirect("register.html");
+      return;
+    }
+
+    global.sess = req.session;
+
+    global.sess.fname = fname;
+    global.sess.lname = lname;
+
+    storeUser(new User(fname, lname, email, pass));
+
+    res.redirect("/");
+    res.end();
+  });
 });
 
 app.post("/login", (req, res) => {
-
   if (global.sess) {
-    if(global.sess.fname) {
+    if (global.sess.fname) {
       res.send("already logged in");
       return;
     }
@@ -77,41 +94,37 @@ app.post("/login", (req, res) => {
   let success = false;
   global.sess = req.session;
 
-  const db = require('./app/scripts/database-init');
-  db.get(
-    "SELECT * FROM Users WHERE email='" + email + "'",
-    (err, row) => {
-      if(err != undefined) {
-        console.log(err);
-        res.send("an error occured");
-        return;
-      }
-
-      if(row) {
-        if(row.password == pass) {
-          global.sess.fname = row.firstName;
-          global.sess.lname = row.lastName;
-
-          success = true;
-        }
-  
-        if(success === false) {
-          console.log("wrong pass word '" + pass + "' (should be '" + row.password + "'");
-          res.redirect("debugLogin.html");
-        }
-        else {
-          res.redirect("/");
-        }
-      
-        res.end();
-      }
-      else {
-        console.log("row not found");
-        res.redirect("debugLogin.html");
-      }
-
+  const db = require("./app/scripts/database-init");
+  db.get("SELECT * FROM Users WHERE email='" + email + "'", (err, row) => {
+    if (err != undefined) {
+      console.log(err);
+      res.send("an error occured");
+      return;
     }
-  );
-})
+
+    if (row) {
+      if (row.password == pass) {
+        global.sess.fname = row.firstName;
+        global.sess.lname = row.lastName;
+
+        success = true;
+      }
+
+      if (success === false) {
+        console.log(
+          "wrong pass word '" + pass + "' (should be '" + row.password + "'"
+        );
+        res.redirect("debugLogin.html");
+      } else {
+        res.redirect("/");
+      }
+
+      res.end();
+    } else {
+      console.log("row not found");
+      res.redirect("debugLogin.html");
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- app/scripts/dc-router.js
+++ app/scripts/dc-router.js
@@ -1,30 +1,28 @@
 /**
- * This is the dynamic content router file. It defines a router which sends the correct JS code 
+ * This is the dynamic content router file. It defines a router which sends the correct JS code
  * to the client, based on whether or not the user is logged in. We can then use this JS code on the client side
  * to build up the dynamic website (using DOM manipulation).
  */
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 router.use((req, res, next) => {
-    if(global.sess) {
-        if (global.sess.fname) {
-            req.dir = __dirname + "/dynamic_content/loggedin";
-        }
-        else {
-            console.log("session defined but no fname " + global.sess.fname);
-            req.dir = __dirname + "/dynamic_content/notloggedin";
-        }
+  if (global.sess) {
+    if (global.sess.fname) {
+      req.dir = __dirname + "/dynamic_content/loggedin";
+    } else {
+      //console.log("session defined but no fname " + global.sess.fname);
+      req.dir = __dirname + "/dynamic_content/notloggedin";
     }
-    else {
-        req.dir = __dirname + "/dynamic_content/notloggedin";
-    }
+  } else {
+    req.dir = __dirname + "/dynamic_content/notloggedin";
+  }
 
-    next();
-})
+  next();
+});
 
 router.get("*", (req, res) => {
-    res.sendFile(req.dir + req.path);
+  res.sendFile(req.dir + req.path);
 });
 
 module.exports = router;
